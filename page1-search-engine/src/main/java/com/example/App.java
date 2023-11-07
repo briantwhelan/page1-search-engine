0fd3131ce29da66285dda692ae2f20145bf941a9
Ming Shi <1945402109@qq.com>
2023-11-07 17:57:05 +0000

    fbisp

--- page1-search-engine/src/main/java/com/example/App.java
+++ page1-search-engine/src/main/java/com/example/App.java
@@ -1,13 +1,19 @@
 package com.example;
 
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    public static void main( String[] args ) throws IOException {
+        FBIS96_parser my_fbis_parser = new FBIS96_parser();
+        my_fbis_parser.load_file();
+
+
+
     }
 }
---
+++ page1-search-engine/src/main/java/com/example/FBIS_text_augment.java
@@ -0,0 +1,42 @@
+package com.example;
+
+public class FBIS_text_augment {
+    private String input_string;
+
+
+    public FBIS_text_augment(String input_string) {
+        this.input_string = input_string;
+    }
+
+    public String augment_string()
+    {
+        String text_content = this.input_string;
+        String text_content_new = text_content.replaceAll("&hyph;", "-");
+        // Remove content between [ and ] except when it is [Text]
+        String replacedString = text_content_new.replaceAll("\\[(?!Text\\]).*?\\]", "[]");
+
+        String remove_left_square_bracket = replacedString.replaceAll("\\[","");
+        String remove_right_square_bracket = remove_left_square_bracket.replaceAll("\\]","");
+        String start_pattern = "Text";
+        String remove_string_content = remove_right_square_bracket.replace(start_pattern, "");
+
+        String remove_delimiter = remove_string_content.replaceAll("-","");
+        String remove_tringle_tag = remove_delimiter.replaceAll("<[^>]*>", "");
+        String remove_language_tage = remove_tringle_tag.replaceAll("Language: ","");
+        //Article Type:BFN
+
+        String remove_bfn_one = remove_language_tage.replaceAll("Article Type:BFN","");
+        String remove_mulit_space = remove_bfn_one.replaceAll("\\s+", " ");
+        String remove_bfn_two = remove_mulit_space.replaceAll("BFN","");
+
+
+
+        String final_string_text = remove_bfn_two;
+
+
+       // System.out.println(final_string_text);
+
+        return final_string_text;
+
+    }
+}
