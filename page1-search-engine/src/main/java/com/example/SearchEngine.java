ed6d42374b1bc0e37d6c87a11284f51199a1d6ff
Ming Shi <1945402109@qq.com>
2023-11-09 16:57:59 +0000

    newFBIS

--- page1-search-engine/src/main/java/com/example/SearchEngine.java
+++ page1-search-engine/src/main/java/com/example/SearchEngine.java
@@ -1,7 +1,7 @@
 package com.example;
 
 import com.example.ft.FinancialTimesReader;
-
+import com.example.fbis_parser.*;
 /**
  * SearchEngine indexes and searches documents.
  */
@@ -13,5 +13,11 @@ public class SearchEngine {
    */
   public static void main(String[] args) throws Exception {
     FinancialTimesReader.readDocuments(FT_PATH);
+
+    Fbis96Parser my_fbis_parser = new Fbis96Parser();
+    my_fbis_parser.loadFiles();
+    // Return a FBIS contain a class
+    // DOCNO,title and text
+    ArrayList<Fbis95Structure> my_FBIS_collection = my_fbis_parser.getMyFbisContainer();
   }
 }
