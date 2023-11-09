59852757fb100d473dc3e978e37e5b5d5932f52d
BloodBoy0117 <xin951586@gmail.com>
2023-11-09 21:34:17 +0000

    Your commit message

--- page1-search-engine/src/main/java/com/example/App.java
+++ page1-search-engine/src/main/java/com/example/App.java
@@ -1,13 +1,41 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class App {
+
+    private static final Path OUTPUT_PATH = Paths.get("output.txt");
+
+    public static void main(String[] args) {
+        QueryParser queryParser = new QueryParser();
+        List<Query> queries = queryParser.parseQueries();
+
+        // Debugging: Print the number of queries found
+        System.out.println("Number of queries parsed: " + queries.size());
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_PATH.toFile()))) {
+            for (Query query : queries) {
+                writer.write("Query Number: " + query.getNumber());
+                writer.newLine();
+                writer.write("Title: " + query.getTitle());
+                writer.newLine();
+                writer.write("Description: " + query.getDescription());
+                writer.newLine();
+                writer.write("Narrative: " + query.getNarrative());
+                writer.newLine();
+                writer.write("---------------------------------------------------");
+                writer.newLine();
+
+                // Debugging: Print each query's title to the console
+                System.out.println("Writing query: " + query.getTitle());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
